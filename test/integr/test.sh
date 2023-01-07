64f6335d3a0556cc1f905835ba417933c2c57128
Vladimir Sulovyev <vsulovyev@gmail.com>
2023-01-07 15:56:56 +0300

    add missing test

--- test/integr/test.sh
+++ test/integr/test.sh
@@ -37,6 +37,17 @@ else
     echo "${RED}[  FAILED  ]${NC} test ${TEST_N}"
 fi
 
+
+TEST_N=$((${TEST_N} + 1))
+echo "${GREEN}[ RUN      ]${NC} test ${TEST_N}"
+if echo "2 t 10 meow t 11 @astolfo t" | ./tmp/demo 2>${NULL} 1>${NULL} == "11 @astolfo"  ; then
+    echo "${GREEN}[       OK ]${NC} test ${TEST_N}"
+    PASSED=$((${PASSED} + 1))
+else
+    echo "${RED}[  FAILED  ]${NC} test ${TEST_N}"
+fi
+
+
 echo "${GREEN}[==========]${NC}"
 if [ ${PASSED} -ne 0 ] ; then
 echo "${GREEN}[  PASSED  ]${NC} ${PASSED} tests."
